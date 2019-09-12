4b99d109b8f5087fb9599a6effbc349ab4755a6d
saxenaharsh <harsh.saxena@gmail.com>
2019-09-12 17:46:12 +0530

    Add files via upload

---
+++ ForLoop/src/com/HarshSaxena/Main.java
@@ -0,0 +1,23 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));*/
+
+        for(int i = 2; i < 9; i++){
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        for(int i = 8; i >= 2; i--){
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+    }
+    public static double calculateInterest(double amount, double interestRate){
+        return(amount * (interestRate/100));
+    }
+}
